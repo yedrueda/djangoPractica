f024423a21644fcf28044599e7115b89e9823e4b
Yelkin <yelkindavid1997@gmail.com>
2025-12-23 17:00:52 -0400

    Configuración inicial: Django + Postgres + App Tareas

---
+++ tareas/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+class Tarea(models.Model):
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    completada = models.BooleanField(default=False)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
