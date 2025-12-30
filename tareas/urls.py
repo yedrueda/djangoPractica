75f57abc51e804e9df531d92a8b793beae094544
yedrueda <yelkindavid1997@gmail.com>
2025-12-30 17:02:38 -0400

    Feat: Agregada lógica CRUD completa (Borrar y Completar tareas)

--- tareas/urls.py
+++ tareas/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.lista_tareas, name='lista_tareas'),
     path('crear/', views.crear_tarea, name='crear_tarea'),
+    path('eliminar/<int:tarea_id>/', views.eliminar_tarea, name='eliminar_tarea'),
+    path('completar/<int:tarea_id>/', views.completar_tarea, name='completar_tarea'),
 ]
--- tareas/views.py
+++ tareas/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from .models import Tarea
 from .forms import TareaForm
+from django.shortcuts import render, redirect, get_object_or_404
 
 def lista_tareas(request):
     tareas = Tarea.objects.all()
@@ -20,3 +21,16 @@ def crear_tarea(request):
         form = TareaForm()
     
     return render(request, 'tareas/crear_tarea.html', {'form': form})
+
+def eliminar_tarea(request, tarea_id):
+    #busca las tareas por id, si no existe devuelve error 404
+    tarea = get_object_or_404(Tarea, id=tarea_id)
+    tarea.delete()
+    return redirect('lista_tareas')
+
+def completar_tarea(request, tarea_id):
+    tarea = get_object_or_404(Tarea, id=tarea_id)
+    tarea.completada = True # cambiamos el estado de la tarea
+    tarea.save() # guardamos los cambios en la base de datos
+    return redirect('lista_tareas')
+
