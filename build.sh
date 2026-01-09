a5a8282367bc31dee5244a1b827e4fa7c6ee0b36
yelkindavid1997 <yelkindavid1997@gmail.com>
2026-01-09 14:36:43 +0000

    delete script

--- build.sh
+++ build.sh
@@ -11,19 +11,4 @@ python manage.py collectstatic --no-input
 # 3. Crear las tablas en la base de datos de la nube
 python manage.py migrate
 
-# Crear o actualizar superusuario automáticamente
-# Esto usa un comando de Python para no pedir interacción de teclado
-python manage.py shell <<EOF
-from django.contrib.auth.models import User
-username = 'admin'
-email = 'yelkindavid1997@gmail.com'
-password = 'admin'
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username, email, password)
-    print("Superusuario creado con éxito")
-else:
-    user = User.objects.get(username=username)
-    user.set_password(password)
-    user.save()
-    print("Password de superusuario actualizado")
-EOF
+
