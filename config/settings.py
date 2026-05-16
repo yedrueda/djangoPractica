95733dfe00ec3383c02e36f9475d66bdb3290d89
YD <yelkindavid1997@gmail.com>
2026-05-16 16:13:36 -0400

    Configuración para despliegue en Render (Gunicorn, Static, Build)

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-6qf$z5gh&k0-6%1=7y8s-wwcc%8jymz7-9sfkyy$^i3g_bghul
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', 'https://sige-cdi.onrender.com'] # dominio exacto que te dio Render
 
 
 # Application definition
@@ -123,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
--- gestion_cdi/admin.py
+++ gestion_cdi/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import PersonalCDI, Paciente, ControlEndocrino, RutaMaterna, PlanificacionFamiliar, InventarioEquipo
 
-# Register your models here.
+# Registramos los módulos base de forma sencilla primero
+admin.site.register(PersonalCDI)
+admin.site.register(Paciente)
+admin.site.register(ControlEndocrino)
+admin.site.register(RutaMaterna)
+admin.site.register(PlanificacionFamiliar)
+admin.site.register(InventarioEquipo)
+
+# Personalizamos el título del Panel
+admin.site.site_header = "Administración del CDI"
+admin.site.site_title = "CDI Portal"
+admin.site.index_title = "Bienvenido al Sistema Integral del CDI"
